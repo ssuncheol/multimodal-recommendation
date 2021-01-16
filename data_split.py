6b452dfa0c2c02adef7e89239f778357ee4ad1ee
2yejin2 <62421163+2yejin2@users.noreply.github.com>
2021-01-17 02:49:25 +0900

    Update data_split.py

--- data_split.py
+++ data_split.py
@@ -9,13 +9,11 @@ parser.add_argument('--data_path', default='./movielens_dataset/ratings_movielen
                     help='datapath')
 parser.add_argument('--save_path', default='./movielens_dataset', type=str,
                     help='savepath')
-parser.add_argument('--split_type', default='leave', type=str,
-                    help='leave/ratio')
 args = parser.parse_args()
 random.seed(1)
 
-import pdb;pdb.set_trace()
 ratings=pd.read_csv(args.data_path)
+type=['leave-one-out','ratio-split']
 # indexing user
 user_id=ratings[['userid']].drop_duplicates().reset_index(drop=True)
 user_id['useridx']=pd.DataFrame(np.arange(len(user_id)))
@@ -36,27 +34,33 @@ rating2=ratings.groupby('userid')['timestamp'].apply(list).reset_index()
 ratings=pd.concat([rating1[['userid', 'itemid']], rating2['timestamp']], axis=1)
 
 # split
-if args.split_type=='leave':
-    ratings['test_positive_index']=ratings['timestamp'].apply(lambda x: np.array(x).argmax())
-    ratings['test_positive']=ratings.apply(lambda x: x['itemid'][x['test_positive_index']], axis=1)
-    ratings['negative']=ratings['itemid'].apply(lambda x: list(items_set - set(x)))
-    ratings['test_negative']=ratings['negative'].apply(lambda x: random.sample(x, 99))
-    ratings['train_negative']=ratings.apply(lambda x: list(items_set - set(x['itemid']) - set(x['test_negative'])), axis=1)
-    ratings.apply(lambda x: x['itemid'].remove(x['test_positive']), axis=1)
+for split_type in type:
+    if split_type=='leave-one-out':
+        ratings['test_positive_index']=ratings['timestamp'].apply(lambda x: np.array(x).argmax())
+        ratings['test_positive']=ratings.apply(lambda x: x['itemid'][x['test_positive_index']], axis=1)
+        ratings['negative']=ratings['itemid'].apply(lambda x: list(items_set - set(x)))
+        ratings['test_negative']=ratings['negative'].apply(lambda x: random.sample(x, 99))
+        ratings['train_negative']=ratings.apply(lambda x: list(items_set - set(x['itemid']) - set(x['test_negative'])), axis=1)
+        ratings.apply(lambda x: x['itemid'].remove(x['test_positive']), axis=1)
 
-elif args.split_type=='ratio':
-    ratings['test_positive']=ratings['itemid'].apply(lambda x: random.sample(x, round(len(x)*0.3)))
-    ratings['itemid']=ratings.apply(lambda x: list(set(x['itemid'])-set(x['test_positive'])), axis=1)
-    ratings['train_negative']=ratings.apply(lambda x: list(items_set - set(x['itemid'])),axis=1)
-    ratings['test_negative']=ratings.apply(lambda x: list(items_set - set(x['itemid'])- set(x['test_positive'])), axis=1)
+    elif split_type=='ratio-split':
+        ratings['test_positive']=ratings['itemid'].apply(lambda x: random.sample(x, round(len(x)*0.3)))
+        ratings['itemid']=ratings.apply(lambda x: list(set(x['itemid'])-set(x['test_positive'])), axis=1)
+        ratings['train_negative']=ratings.apply(lambda x: list(items_set - set(x['itemid'])),axis=1)
+        ratings['test_negative']=ratings.apply(lambda x: list(items_set - set(x['itemid'])- set(x['test_positive'])), axis=1)
 
-ratings.rename(columns = {'itemid':'train_positive'}, inplace = True)
-ratings = ratings[['userid','train_positive','test_positive','train_negative', 'test_negative']].reset_index(drop=True)
-train_positive=ratings.join(ratings['train_positive'].apply(lambda x:pd.Series(x)).stack().reset_index(1,name='train_pos').drop('level_1',axis=1))
-test_positive=ratings.join(ratings['test_positive'].apply(lambda x:pd.Series(x)).stack().reset_index(1,name='test_pos').drop('level_1',axis=1))
+    ratings.rename(columns = {'itemid':'train_positive'}, inplace = True)
+    ratings = ratings[['userid','train_positive','test_positive','train_negative', 'test_negative']].reset_index(drop=True)
+    train_positive=ratings.join(ratings['train_positive'].apply(lambda x:pd.Series(x)).stack().reset_index(1,name='train_pos').drop('level_1',axis=1))
+    test_positive=ratings.join(ratings['test_positive'].apply(lambda x:pd.Series(x)).stack().reset_index(1,name='test_pos').drop('level_1',axis=1))
 
-# save
-train_positive[['userid','train_pos']].to_csv(os.path.join(args.save_path, 'train_positive.csv'),index=False)
-test_positive[['userid','test_pos']].to_csv(os.path.join(args.save_path, 'test_positive.csv'),index=False)
-ratings[['userid','train_negative']].to_csv(os.path.join(args.save_path, 'train_negative.csv'),index=False)
-ratings[['userid','test_negative']].to_csv(os.path.join(args.save_path, 'test_negative.csv'),index=False)
+    # save
+    train_positive[['userid','train_pos']].to_csv(os.path.join(args.save_path,split_type, 'train_positive.csv'),index=False)
+    test_positive[['userid','test_pos']].to_csv(os.path.join(args.save_path,split_type, 'test_positive.csv'),index=False)
+    ratings[['userid','train_negative']].to_csv(os.path.join(args.save_path,split_type, 'train_negative.csv'),index=False)
+    ratings[['userid','test_negative']].to_csv(os.path.join(args.save_path,split_type, 'test_negative.csv'),index=False)
+
+if not os.path.exists(os.path.join(args.save_path,'index-info')):
+    os.makedirs(os.path.join(args.save_path,'index-info'))
+user_id.to_csv(os.path.join(args.save_path,'index-info', 'user_index.csv'),index=False)
+item_id.to_csv(os.path.join(args.save_path,'index-info', 'item_index.csv'),index=False)
