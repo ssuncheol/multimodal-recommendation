367917e1ca03b2d303593a2d7320c3b5ce3bbfe6
seungwon <69955858+tjtmddnjswkd@users.noreply.github.com>
2021-02-08 16:34:26 +0900

    Update evaluate.py

--- NCF/evaluate.py
+++ NCF/evaluate.py
@@ -3,26 +3,22 @@ import time
 from metric import get_performance
 import numpy as np
 
-
-
 class Engine(object):
     def __init__(self, top_k):
         self.top_k = top_k
         
     def evaluate(self, model, test_loader, epoch_id, **kwargs):
         #Evaluate model
-        a=time.time()
+        t0 = time.time()
         if (epoch_id + 1) % kwargs['interval'] != 0:
             return 0, 0, 0
         
-        model.eval()
+        # model.eval()
 
         hr_list = []
         hr2_list = []
         ndcg_list = []
-        
         for i , data in enumerate(test_loader): 
-
             with torch.no_grad():    
                 if ('image' in kwargs.keys()) & ('text' in kwargs.keys()):
                     user, item, image_f, text_f, label = data
@@ -61,6 +57,6 @@ class Engine(object):
         hit_ratio, hit_ratio2, ndcg = np.mean(hr_list), np.mean(hr2_list), np.mean(ndcg_list)
         print('[Evluating Epoch {}] HR = {:.4f}, HR2 = {:.4f}, NDCG = {:.4f}'.format(epoch_id+1, hit_ratio, hit_ratio2, ndcg))
         
-        b=time.time()
-        print("evaluate time:",b-a)  
+        t1 = time.time()
+        print("evaluate time:", t1 - t0)  
         return hit_ratio, hit_ratio2, ndcg
