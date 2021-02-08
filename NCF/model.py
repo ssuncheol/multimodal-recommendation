cc0c09bcf676abea9d3667cf38443ec8cdc95eb5
seungwon <69955858+tjtmddnjswkd@users.noreply.github.com>
2021-02-08 16:30:17 +0900

    Update model.py

--- NCF/model.py
+++ NCF/model.py
@@ -1,11 +1,12 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import resnet_tv
 
 class NeuralCF(nn.Module):
-    def __init__(self, num_users, num_items, embedding_size, dropout, num_layers, **kwargs):
+    def __init__(self, num_users, num_items, embedding_size, dropout, num_layers, feature, **kwargs):
         super(NeuralCF,self).__init__()
-    
+        self.feature = feature
         self.user_embedding_gmf = nn.Embedding(num_users, embedding_size)
         self.item_embedding_gmf = nn.Embedding(num_items, embedding_size)        
         self.user_embedding_mlp = nn.Embedding(num_users, embedding_size)
@@ -13,10 +14,12 @@ class NeuralCF(nn.Module):
         
         if ('image' in kwargs.keys()):
             print("IMAGE FEATURE")
-            self.image_embedding = nn.Linear(kwargs["image"], embedding_size) # Image Embedding
+            if self.feature == 'raw':
+                self.feature_extract = resnet_tv.resnet18().eval()
+            self.image_embedding = nn.Linear(kwargs["image"], embedding_size) 
         if ('text' in kwargs.keys()):
             print("TEXT FEATURE")
-            self.text_embedding = nn.Linear(kwargs["text"], embedding_size) # Text Embedding
+            self.text_embedding = nn.Linear(kwargs["text"], embedding_size)
  
         #mlp module 
         MLP_modules = []
@@ -25,6 +28,7 @@ class NeuralCF(nn.Module):
             for i in range(num_layers):
                 input_size = 4 * embedding_size // (2 ** i)
                 MLP_modules.append(nn.Linear(input_size, input_size // 2))
+                MLP_modules.append(nn.BatchNorm1d(input_size // 2))
                 MLP_modules.append(nn.ReLU())
                 MLP_modules.append(nn.Dropout(p=dropout))
         elif ('image' in kwargs.keys()) | ('text' in kwargs.keys()):
@@ -32,6 +36,7 @@ class NeuralCF(nn.Module):
             for i in range(num_layers):
                 input_size = 3 * embedding_size // (2 ** i)
                 MLP_modules.append(nn.Linear(input_size, input_size // 2))
+                MLP_modules.append(nn.BatchNorm1d(input_size // 2))
                 MLP_modules.append(nn.ReLU())
                 MLP_modules.append(nn.Dropout(p=dropout))
         else:
@@ -39,6 +44,7 @@ class NeuralCF(nn.Module):
             for i in range(num_layers):
                 input_size = 2 * embedding_size // (2 ** i)  
                 MLP_modules.append(nn.Linear(input_size, input_size//2))
+                MLP_modules.append(nn.BatchNorm1d(input_size // 2))
                 MLP_modules.append(nn.ReLU())
                 MLP_modules.append(nn.Dropout(p=dropout))
 
@@ -59,27 +65,28 @@ class NeuralCF(nn.Module):
         nn.init.normal_(self.user_embedding_mlp.weight, std=0.01)
         nn.init.normal_(self.item_embedding_gmf.weight, std=0.01)
         nn.init.normal_(self.item_embedding_mlp.weight, std=0.01)
-    
-
         
     def forward(self,user_indices,item_indices,**kwargs):
-        user_gmf=self.user_embedding_gmf(user_indices)
-        item_gmf=self.item_embedding_gmf(item_indices)
+        user_gmf = self.user_embedding_gmf(user_indices)
+        item_gmf = self.item_embedding_gmf(item_indices)
         
-        user_mlp=self.user_embedding_mlp(user_indices)
-        item_mlp=self.item_embedding_mlp(item_indices)
+        user_mlp = self.user_embedding_mlp(user_indices)
+        item_mlp = self.item_embedding_mlp(item_indices)
         
         if ('image' in kwargs.keys()):
-            image=F.relu(self.image_embedding(kwargs["image"]))
+            if self.feature == 'raw':
+                _, image = self.feature_extract.feature_list(kwargs['image'])
+                image = F.relu(self.image_embedding(image[5]))
+            else:
+                image = F.relu(self.image_embedding(kwargs['image']))
             item_mlp = torch.cat([item_mlp,image], -1)
         if ('text' in kwargs.keys()):
-            text=F.relu(self.text_embedding(kwargs["text"]))
+            text = F.relu(self.text_embedding(kwargs["text"]))
             item_mlp = torch.cat([item_mlp,text], -1)
-            
         
-        gmf=torch.mul(user_gmf, item_gmf)
-        mlp=torch.cat([user_mlp, item_mlp], -1)
+        gmf = torch.mul(user_gmf, item_gmf)
+        mlp = torch.cat([user_mlp, item_mlp], -1)
         mlp = self.MLP_layers(mlp)
-        x=torch.cat((gmf, mlp), 1)
-        x=self.predict_layer(x)
+        x = torch.cat((gmf, mlp), 1)
+        x = self.predict_layer(x)
         return x.view(-1)
