5bd61d04a8658c37e1b50997d1faeab0374348d1
2yejin2 <62421163+2yejin2@users.noreply.github.com>
2021-05-24 23:12:16 +0900

    Update model.py

--- NCF_MAML/model.py
+++ NCF_MAML/model.py
@@ -1,24 +1,36 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import resnet_tv as resnet
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import resnet_tv
+from bam import *
+
 
 class NeuralCF(nn.Module):
-    def __init__(self, num_users, num_items, embedding_size, dropout, num_layers, **kwargs):
-        super(NeuralCF,self).__init__()
+    def __init__(self, num_users, num_items, embedding_size, dropout, num_layers, att_type, **kwargs):
+        super(NeuralCF, self).__init__()
         self.user_embedding_gmf = nn.Embedding(num_users, embedding_size)
-        self.item_embedding_gmf = nn.Embedding(num_items, embedding_size)        
+        self.item_embedding_gmf = nn.Embedding(num_items, embedding_size)
         self.user_embedding_mlp = nn.Embedding(num_users, embedding_size)
         self.item_embedding_mlp = nn.Embedding(num_items, embedding_size)
 
+        # for bam
+        self.att_type = att_type
+        if self.att_type == 'BAM':
+            self.bam1 = BAM(64)
+            self.bam2 = BAM(128)
+            self.bam3 = BAM(256)
+        else:
+            self.bam1, self.bam2, self.bam3 = None, None, None
+
         if (kwargs['feature_type'] == 'img') | (kwargs['feature_type'] == 'all'):
             print("IMAGE FEATURE")
+
             self.v_feature_extractor = resnet_tv.resnet18()
-            self.v_feature_extractor.load_state_dict(torch.load(kwargs['extractor_path'], map_location='cuda:%d' % kwargs['rank']))    
+            self.v_feature_extractor.load_state_dict(
+                torch.load(kwargs['extractor_path'], map_location='cuda:%d' % kwargs['rank']))
             # attention을 위한 field들
             self.v_feature_dim = self.v_feature_extractor.fc.in_features
             self.v_feature_c1 = self.v_feature_extractor.conv1.out_channels
@@ -31,7 +43,7 @@ class NeuralCF(nn.Module):
                 self.v_feature_extractor.eval()
                 for param in self.v_feature_extractor.parameters():
                     param.requires_grad = False
-            
+
             self.image_embedding = []
             self.image_embedding.append(nn.Linear(self.v_feature_extractor.fc.in_features, 256))
             self.image_embedding.append(nn.BatchNorm1d(256))
@@ -57,7 +69,7 @@ class NeuralCF(nn.Module):
             self.text_embedding.append(nn.ReLU())
             self.text_embedding = nn.Sequential(*self.text_embedding)
 
-        # mlp module 
+        # mlp module
         if kwargs['feature_type'] == 'all':
             input_size = 4 * embedding_size
             print("MLP FEATURE 2")
@@ -65,25 +77,22 @@ class NeuralCF(nn.Module):
             input_size = 3 * embedding_size
             print("MLP FEATURE 1")
         else:
-            input_size = 2 * embedding_size 
+            input_size = 2 * embedding_size
             print("MLP FEATURE 0")
 
-        if num_layers == 4:
-            MLP_dim = [input_size, input_size, input_size//2, input_size//2, embedding_size]
-        else:
-            MLP_dim = [input_size, input_size, input_size//2, embedding_size]
+        MLP_dim = [input_size, input_size, input_size // 2, input_size // 2, embedding_size]
         MLP_modules = []
         for i in range(num_layers):
             if i == 0:
-                MLP_modules.append(nn.Linear(input_size, int(MLP_dim[i+1])))
+                MLP_modules.append(nn.Linear(input_size, int(MLP_dim[i + 1])))
             else:
-                MLP_modules.append(nn.Linear(int(MLP_dim[i]), int(MLP_dim[i+1])))
-            MLP_modules.append(nn.BatchNorm1d(int(MLP_dim[i+1])))
+                MLP_modules.append(nn.Linear(int(MLP_dim[i]), int(MLP_dim[i + 1])))
+            MLP_modules.append(nn.BatchNorm1d(int(MLP_dim[i + 1])))
             MLP_modules.append(nn.ReLU())
             MLP_modules.append(nn.Dropout(p=dropout))
-                
-        self.MLP_layers =nn.Sequential(*MLP_modules)
-        
+
+        self.MLP_layers = nn.Sequential(*MLP_modules)
+
         # Predict layer
         self.predict_layer = nn.Linear(embedding_size * 2, 1)
 
@@ -106,8 +115,18 @@ class NeuralCF(nn.Module):
         nn.init.normal_(self.user_embedding_mlp.weight, std=0.01)
         nn.init.normal_(self.item_embedding_gmf.weight, std=0.01)
         nn.init.normal_(self.item_embedding_mlp.weight, std=0.01)
-    
-    def hierarchical_attention(self, v_feature_extractor, key_modules, value_modules, image, user_embedding, item_embedding):
+
+    def bam_attention(self, feature_map):
+        if not self.bam1 is None:
+            feature_map[1] = self.bam1(feature_map[1])
+        if not self.bam2 is None:
+            feature_map[2] = self.bam2(feature_map[2])
+        if not self.bam3 is None:
+            feature_map[3] = self.bam3(feature_map[3])
+        return feature_map
+
+    def hierarchical_attention(self, v_feature_extractor, key_modules, value_modules, image, user_embedding,
+                               item_embedding):
         if len(user_embedding.size()) == 3:
             user_embedding = user_embedding.reshape(user_embedding.size(0) * user_embedding.size(1),
                                                     user_embedding.size(2))
@@ -115,6 +134,7 @@ class NeuralCF(nn.Module):
                                                     item_embedding.size(2))
         user_embedding = torch.cat([user_embedding, item_embedding], 1)
         _, feature_map = v_feature_extractor.feature_list(image)
+        feature_map = self.bam_attention(feature_map)
         feature_map = feature_map[:-1]
         score = []
         for i in range(len(feature_map)):
@@ -131,30 +151,32 @@ class NeuralCF(nn.Module):
                 attention_matrix += torch.mul(score[i].unsqueeze(0).T, value.squeeze())
 
         return attention_matrix
-    
+
     def forward(self, user_indices, item_indices, **kwargs):
         key_modules = [self.conv_key1, self.conv_key2, self.conv_key3, self.conv_key4, self.conv_key5]
         value_modules = [self.conv_value1, self.conv_value2, self.conv_value3, self.conv_value4, self.cnov_value5]
 
         user_gmf = self.user_embedding_gmf(user_indices)
         item_gmf = self.item_embedding_gmf(item_indices)
-        
+
         user_mlp = self.user_embedding_mlp(user_indices)
         item_mlp = self.item_embedding_mlp(item_indices)
 
         if (kwargs['feature_type'] == 'img') | (kwargs['feature_type'] == 'all'):
             if kwargs['hier_attention']:
-                image = self.hierarchical_attention(self.v_feature_extractor, key_modules, value_modules, kwargs['image'],
-                                                        user_mlp, item_mlp)
+                image = self.hierarchical_attention(self.v_feature_extractor, key_modules, value_modules,
+                                                    kwargs['image'],
+                                                    user_mlp, item_mlp)
                 image = self.image_embedding(image)
             else:
                 _, image = self.v_feature_extractor.feature_list(kwargs['image'])
+                image = self.bam_attention(image)
                 image = self.image_embedding(image[5])
             item_mlp = torch.cat([item_mlp, image], -1)
         if (kwargs['feature_type'] == 'txt') | (kwargs['feature_type'] == 'all'):
             text = self.text_embedding(kwargs["text"])
-            item_mlp = torch.cat([item_mlp,text], -1)
-        
+            item_mlp = torch.cat([item_mlp, text], -1)
+
         gmf = torch.mul(user_gmf, item_gmf)
         mlp = torch.cat([user_mlp, item_mlp], -1)
         mlp = self.MLP_layers(mlp)
@@ -162,9 +184,10 @@ class NeuralCF(nn.Module):
         x = self.predict_layer(x)
         return x.view(-1)
 
+
 class MAML(nn.Module):
     def __init__(self, n_users, n_items, embed_dim, dropout_rate, feature_type, t_feature_dim,
-                 v_feature_extractor_path, fine_tuning, rank):
+                 v_feature_extractor_path, fine_tuning, rank, att_type):
         super(MAML, self).__init__()
         self.embed_dim = embed_dim
         self.n_users = n_users
@@ -173,13 +196,14 @@ class MAML(nn.Module):
         self.feature_type = feature_type
         self.t_feature_dim = t_feature_dim
         self.rank = rank
+        self.att_type = att_type
 
         # Embedding Layers
         self.embedding_user = nn.Embedding(n_users, embed_dim, max_norm=1.0)
         self.embedding_item = nn.Embedding(n_items, embed_dim, max_norm=1.0)
 
         # Image feature extractor module
-        self.v_feature_extractor = resnet.resnet18()
+        self.v_feature_extractor = resnet_tv.resnet18()
         self.v_feature_dim = self.v_feature_extractor.fc.in_features
         self.v_feature_c1 = self.v_feature_extractor.conv1.out_channels
         self.v_feature_c2 = self.v_feature_extractor.layer1[1].conv2.out_channels
@@ -197,17 +221,24 @@ class MAML(nn.Module):
                 param.requires_grad = False
 
         # For attention Layers
-        self.conv_key1 = nn.Conv2d(self.v_feature_c1, self.embed_dim*2, 1)
-        self.conv_key2 = nn.Conv2d(self.v_feature_c2, self.embed_dim*2, 1)
-        self.conv_key3 = nn.Conv2d(self.v_feature_c3, self.embed_dim*2, 1)
-        self.conv_key4 = nn.Conv2d(self.v_feature_c4, self.embed_dim*2, 1)
-        self.conv_key5 = nn.Conv2d(self.v_feature_c5, self.embed_dim*2, 1)
+        self.conv_key1 = nn.Conv2d(self.v_feature_c1, self.embed_dim * 2, 1)
+        self.conv_key2 = nn.Conv2d(self.v_feature_c2, self.embed_dim * 2, 1)
+        self.conv_key3 = nn.Conv2d(self.v_feature_c3, self.embed_dim * 2, 1)
+        self.conv_key4 = nn.Conv2d(self.v_feature_c4, self.embed_dim * 2, 1)
+        self.conv_key5 = nn.Conv2d(self.v_feature_c5, self.embed_dim * 2, 1)
 
         self.conv_value1 = nn.Conv2d(self.v_feature_c1, self.v_feature_dim, 1)
         self.conv_value2 = nn.Conv2d(self.v_feature_c2, self.v_feature_dim, 1)
         self.conv_value3 = nn.Conv2d(self.v_feature_c3, self.v_feature_dim, 1)
         self.conv_value4 = nn.Conv2d(self.v_feature_c4, self.v_feature_dim, 1)
         self.cnov_value5 = nn.Conv2d(self.v_feature_c5, self.v_feature_dim, 1)
+
+        if self.att_type == 'BAM':
+            self.bam1 = BAM(64)
+            self.bam2 = BAM(128)
+            self.bam3 = BAM(256)
+        else:
+            self.bam1, self.bam2, self.bam3 = None, None, None
 
         # Feature Fusion Layers
         """
@@ -246,21 +277,32 @@ class MAML(nn.Module):
         self.attention = nn.Sequential(
             nn.Linear(attention_num * embed_dim, attention_num * embed_dim),
             nn.Tanh(),
-            nn.Linear(attention_num * embed_dim, embed_dim*2),
+            nn.Linear(attention_num * embed_dim, embed_dim * 2),
             nn.Tanh(),
-            nn.Linear(embed_dim*2, embed_dim*2),
+            nn.Linear(embed_dim * 2, embed_dim * 2),
             nn.Tanh(),
-            nn.Linear(embed_dim*2 , embed_dim)
+            nn.Linear(embed_dim * 2, embed_dim)
         )
 
-    def hierarchical_attention(self, v_feature_extractor, key_modules, value_modules, image, user_embedding, item_embedding):
+    def bam_attention(self, feature_map):
+        if not self.bam1 is None:
+            feature_map[1] = self.bam1(feature_map[1])
+        if not self.bam2 is None:
+            feature_map[2] = self.bam2(feature_map[2])
+        if not self.bam3 is None:
+            feature_map[3] = self.bam3(feature_map[3])
+        return feature_map
+
+    def hierarchical_attention(self, v_feature_extractor, key_modules, value_modules, image, user_embedding,
+                               item_embedding):
         if len(user_embedding.size()) == 3:
             user_embedding = user_embedding.reshape(user_embedding.size(0) * user_embedding.size(1),
                                                     user_embedding.size(2))
             item_embedding = item_embedding.reshape(item_embedding.size(0) * item_embedding.size(1),
                                                     item_embedding.size(2))
-        user_embedding=torch.cat([user_embedding, item_embedding],1)
+        user_embedding = torch.cat([user_embedding, item_embedding], 1)
         _, feature_map = v_feature_extractor.feature_list(image)
+        feature_map = self.bam_attention(feature_map)
         feature_map = feature_map[:-1]
         score = []
         for i in range(len(feature_map)):
@@ -298,6 +340,7 @@ class MAML(nn.Module):
                                                         p_u, q_i)
             else:
                 _, v_feature = self.v_feature_extractor.feature_list(image)
+                v_feature = self.bam_attention(v_feature)
                 v_feature = v_feature[5]
             if len(item.size()) == 2:
                 v_feature = v_feature.reshape(q_i.size(0), q_i.size(1), -1)
@@ -309,6 +352,7 @@ class MAML(nn.Module):
                                                         p_u, q_i)
             else:
                 _, v_feature = self.v_feature_extractor.feature_list(image)
+                v_feature = self.bam_attention(v_feature)
                 v_feature = v_feature[5]
             if len(item.size()) == 2:
                 v_feature = v_feature.reshape(q_i.size(0), q_i.size(1), -1)
