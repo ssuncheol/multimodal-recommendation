dd8d2a1ccf3ddadfc0cb8a50109af4ca15282fba
seungwon <69955858+tjtmddnjswkd@users.noreply.github.com>
2021-07-14 16:50:15 +0900

    Update main.py

--- NCF_MAML/main.py
+++ NCF_MAML/main.py
@@ -26,7 +26,7 @@ parser.add_argument('--batch_size', default=512, type=int,
                     help='Total batch size')
 parser.add_argument('--epoch', default=50, type=int,
                     help='train epoch')
-parser.add_argument('--data_path', default='/daintlab/data/recommend/Amazon-clothing-raw', type=str,
+parser.add_argument('--data_path', default='/daintlab/data/recommend/Amazon-office-raw', type=str,
                     help='Path to rating data')
 parser.add_argument('--num_layers', default=4, type=int,
                     help='number of used layers in NCF')
@@ -54,7 +54,7 @@ parser.add_argument('--feature_type', default='rating', type=str,
                     help='Type of feature to use. [all, img, txt, rating]')
 parser.add_argument('--eval_type', default='ratio-split', type=str,
                     help='Evaluation protocol. [ratio-split, leave-one-out]')
-parser.add_argument('--cnn_path', default='./pretrained_model/resnet18.pth', type=str,
+parser.add_argument('--cnn_path', default='./resnet18.pth', type=str,
                     help='Path to feature data')
 parser.add_argument('--ddp_port', default='8888', type=str,
                     help='DDP Port')
@@ -64,8 +64,10 @@ parser.add_argument('--fine_tuning', default=False, type=bool,
                     help='Fine tuning')
 parser.add_argument('--hier_attention', default=False, type=bool,
                     help='Hierarchical attention')
+parser.add_argument('--mode', default='train', type=str,
+                    help='mode(train, test)')
 parser.add_argument('--att_type', default=None, type=str)
-parser.add_argument('--att_wd', default=100, type=float)
+parser.add_argument('--att_wd', default=0.1, type=float)
 args = parser.parse_args()
 
 
@@ -84,11 +86,12 @@ def main(rank, args):
     # Load dataset
     print("Loading Dataset")
     data_path = os.path.join(args.data_path, args.eval_type)
-    train_df, test_df, train_ng_pool, test_negative, num_user, num_item, text_feature, images, test_pos_item_num, item_num_dict = D.load_data(
+    train_df, val_df, test_df, train_ng_pool, test_negative, num_user, num_item, text_feature, images, test_pos_item_num, item_num_dict = D.load_data(
         data_path, args.feature_type)
     train_dataset = D.CustomDataset(args.model, train_df, text_feature, images, negative=train_ng_pool,
-                                    num_neg=args.num_neg,
-                                    istrain=True, feature_type=args.feature_type)
+                                    num_neg=args.num_neg, istrain=True, feature_type=args.feature_type)
+    val_dataset = D.CustomDataset(args.model, val_df, text_feature, images, negative=test_negative, num_neg=None,
+                                   istrain=False, feature_type=args.feature_type)
     test_dataset = D.CustomDataset(args.model, test_df, text_feature, images, negative=test_negative, num_neg=None,
                                    istrain=False, feature_type=args.feature_type)
     # Divide batch size by num gpus
@@ -98,27 +101,29 @@ def main(rank, args):
                                                                     rank=rank,
                                                                     num_replicas=args.world_size,
                                                                     shuffle=True)
-    train_loader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=False, num_workers=4,
+    train_loader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=False, num_workers=2,
                               collate_fn=my_collate_trn, pin_memory=True, sampler=train_sampler)
-    test_loader = DataLoader(test_dataset, batch_size=int(args.batch_size / 4), shuffle=False, num_workers=4,
+    val_loader = DataLoader(val_dataset, batch_size=int(args.batch_size / 4), shuffle=False, num_workers=2,
+                             collate_fn=my_collate_tst, pin_memory=True)
+    test_loader = DataLoader(test_dataset, batch_size=int(args.batch_size / 4), shuffle=False, num_workers=2,
                              collate_fn=my_collate_tst, pin_memory=True)
 
     # Model
-    t_feature_dim = text_feature[0].shape[-1]
+    t_feature_dim = 300
     if args.model == 'MAML':
         model = MAML(num_user, num_item, args.embed_dim, args.dropout_rate, args.feature_type, t_feature_dim,
-                     args.cnn_path, args.fine_tuning, rank, args.att_type).cuda(rank)
+                     args.cnn_path, args.fine_tuning, rank, args.att_type, args.hier_attention).cuda(rank)
     else:
         model = NeuralCF(num_users=num_user, num_items=num_item,
                          embedding_size=args.embed_dim, dropout=args.dropout_rate,
                          num_layers=args.num_layers, feature_type=args.feature_type, text=t_feature_dim,
                          extractor_path=args.cnn_path, rank=rank, fine_tuning=args.fine_tuning, att_type=args.att_type).cuda(rank)
 
-    model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[rank], find_unused_parameters=True)
+    model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[rank], find_unused_parameters=True) 
 
     # Load from checkpoint
     if args.load_path is not None:
-        checkpoint = torch.load(args.load_path)
+        checkpoint = torch.load(args.load_path, map_location='cuda:%d' % rank)
         model.load_state_dict(checkpoint, strict=False)
         print("Pretrained Model Loaded")
 
@@ -151,33 +156,43 @@ def main(rank, args):
 
     # Logger
     train_logger = Logger(f'{save_path}/train.log')
+    val_logger = Logger(f'{save_path}/val.log')
     test_logger = Logger(f'{save_path}/test.log')
 
-    # Train & Eval
-    for epoch in range(args.epoch):
+    # Test
+    if args.mode == 'test':
         start = time.time()
-        train_sampler.set_epoch(epoch)
-        if args.model == "MAML":
-            train(model=model, model_type=args.model, optimizer=optimizer,
-                  scaler=scaler, train_loader=train_loader, train_logger=train_logger,
-                  epoch=epoch, embedding_loss=embedding_loss, feature_loss=feature_loss,
-                  covariance_loss=covariance_loss, hier_attention=args.hier_attention)
-        else:
-            train(model=model, model_type=args.model, optimizer=optimizer,
-                  scaler=scaler, train_loader=train_loader, train_logger=train_logger,
-                  epoch=epoch, criterion=criterion, hier_attention=args.hier_attention)
+        epoch = 50000
         if dist.get_rank() == 0:
-            print('epoch time : ', time.time() - start, 'sec/epoch => ', (time.time() - start) / 60, 'min/epoch')
-        # Save and test Model every n epoch
-        if (epoch + 1) % args.eval_freq == 0 or epoch == 0:
-            start = time.time()
-            if dist.get_rank() == 0:
-                torch.save(model.state_dict(), f"{save_path}/model_{epoch + 1}.pth")
-                test(model=model, model_type=args.model, test_loader=test_loader, test_logger=test_logger, epoch=epoch,
-                     test_pos_item_num=test_pos_item_num, item_num_dict=item_num_dict,
-                     hier_attention=args.hier_attention)
-                print('test time : ', time.time() - start, 'sec/epoch => ', (time.time() - start) / 60, 'min')
+            test(model=model, model_type=args.model, test_loader=test_loader, test_logger=test_logger, epoch=epoch, 
+                test_pos_item_num=test_pos_item_num, item_num_dict=item_num_dict, hier_attention=args.hier_attention)
+            print('test time : ', time.time() - start, 'sec/epoch => ', (time.time() - start) / 60, 'min')
 
+    # Train & Eval
+    else:
+        for epoch in range(args.epoch):
+            start = time.time()
+            train_sampler.set_epoch(epoch)
+            if args.model == "MAML":
+                train(model=model, model_type=args.model, optimizer=optimizer,
+                    scaler=scaler, train_loader=train_loader, train_logger=train_logger,
+                    epoch=epoch, embedding_loss=embedding_loss, feature_loss=feature_loss,
+                    covariance_loss=covariance_loss, hier_attention=args.hier_attention)
+            else:
+                train(model=model, model_type=args.model, optimizer=optimizer,
+                    scaler=scaler, train_loader=train_loader, train_logger=train_logger,
+                    epoch=epoch, criterion=criterion, hier_attention=args.hier_attention)
+            if dist.get_rank() == 0:
+                print('epoch time : ', time.time() - start, 'sec/epoch => ', (time.time() - start) / 60, 'min/epoch')
+            # Save and evaluate Model every n epoch
+            if (epoch + 1) % args.eval_freq == 0 or epoch == 0:
+                start = time.time() 
+                if dist.get_rank() == 0:
+                    torch.save(model.state_dict(), f"{save_path}/model_{epoch + 1}.pth")
+                    test(model=model, model_type=args.model, test_loader=val_loader, test_logger=val_logger, epoch=epoch,
+                        test_pos_item_num=test_pos_item_num, item_num_dict=item_num_dict, hier_attention=args.hier_attention)
+                    print('validation time : ', time.time() - start, 'sec/epoch => ', (time.time() - start) / 60, 'min')
+        
     cleanup()
 
 
@@ -295,8 +310,8 @@ def test(model, model_type, test_loader, test_logger, epoch, test_pos_item_num, 
     ndcg_10 = AverageMeter()
     data_time = AverageMeter()
     iter_time = AverageMeter()
-    k = [1, 3, 5, 10]
-
+    k = [1, 10]
+    
     score_cat = torch.tensor([]).cuda(dist.get_rank())
 
     end = time.time()
@@ -313,6 +328,7 @@ def test(model, model_type, test_loader, test_logger, epoch, test_pos_item_num, 
             else:  # NCF
                 score = model(user, item, image=image, text=feature, feature_type=args.feature_type,
                               hier_attention=kwargs['hier_attention'])
+        
             score_cat = torch.cat((score_cat, score))
 
             if (i % 500) == 0 and (dist.get_rank() == 0):
@@ -334,14 +350,6 @@ def test(model, model_type, test_loader, test_logger, epoch, test_pos_item_num, 
                         hr_1.update(performance[0])
                         hr2_1.update(performance[1])
                         ndcg_1.update(performance[2])
-                    elif i == 3:
-                        hr_3.update(performance[0])
-                        hr2_3.update(performance[1])
-                        ndcg_3.update(performance[2])
-                    elif i == 5:
-                        hr_5.update(performance[0])
-                        hr2_5.update(performance[1])
-                        ndcg_5.update(performance[2])
                     else:
                         hr_10.update(performance[0])
                         hr2_10.update(performance[1])
@@ -358,12 +366,9 @@ def test(model, model_type, test_loader, test_logger, epoch, test_pos_item_num, 
         print(
             f"Epoch : [{epoch + 1}/{args.epoch}] Hit Ratio : {hr_10.avg:.4f} nDCG : {ndcg_10.avg:.4f} Hit Ratio 2 : {hr2_10.avg:.4f} Test Time : {iter_time.avg:.4f}/user")
         test_logger.write(
-            [epoch, float(hr_1.avg), float(hr2_1.avg), float(ndcg_1.avg), float(hr_3.avg), float(hr2_3.avg),
-             float(ndcg_3.avg)
-                , float(hr_5.avg), float(hr2_5.avg), float(ndcg_5.avg), float(hr_10.avg), float(hr2_10.avg),
-             float(ndcg_10.avg)])
+            [epoch, float(hr_1.avg), float(hr2_1.avg), float(ndcg_1.avg), float(hr_10.avg), float(hr2_10.avg), float(ndcg_10.avg)])
 
-
+            
 def my_collate_trn(batch):
     # MAML
     if len(batch[0]) == 7:
